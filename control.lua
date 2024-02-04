e61f00a09d94908b446873f0c3194061f08b5444
GBT13 <overbeekvanhenk@gmail.com>
2024-02-04 13:37:04 +0100

    add shortcut toggle

--- control.lua
+++ control.lua
@@ -1,50 +1,62 @@
--- script.on_event(defines.events.on_player_mined_entity, function(event) 
---     local player = game.get_player(event.player_index)
---     local trashInventory = player.get_inventory(defines.inventory.character_trash)
---     local mainInventory = player.get_main_inventory()
+local shortcutEnabled = function(player)
+    return player.is_shortcut_toggled("trash-pickup-toggle")
+end
 
---     for item, count in pairs(event.buffer.get_contents()) do
---         trashInventory.insert({name=item, count=count})
---     end
+script.on_event(defines.events.on_player_mined_entity, function(event) 
+    local player = game.get_player(event.player_index)
+    if not shortcutEnabled(player) then
+        return end
 
---     event.buffer.clear()
--- end)
+    local trashInventory = player.get_inventory(defines.inventory.character_trash)
+    local mainInventory = player.get_main_inventory()
 
--- script.on_event(defines.events.on_picked_up_item, function(event)
---     local player = game.get_player(event.player_index)
---     local trashInventory = player.get_inventory(defines.inventory.character_trash)
---     local mainInventory = player.get_main_inventory()
+    for item, count in pairs(event.buffer.get_contents()) do
+        trashInventory.insert({name=item, count=count})
+    end
 
+    event.buffer.clear()
+end)
 
---     mainInventory.remove(event.item_stack)
---     trashInventory.insert(event.item_stack)
--- end)
+script.on_event(defines.events.on_picked_up_item, function(event)
+    local player = game.get_player(event.player_index)
+    if not shortcutEnabled(player) then
+        return end
 
--- script.on_event(defines.events.on_player_main_inventory_changed, function(event)
---     local player = game.get_player(event.player_index)
---     local trashInventory = player.get_inventory(defines.inventory.character_trash)
---     local mainInventory = player.get_main_inventory()
---     local inventoryContent = mainInventory.get_contents()
---     local diff = nil
-
---     for item, amount in pairs (inventoryContent) do
---         if playerInventories[event.player_index][item] ~= nil then
---             diff = amount - playerInventories[event.player_index][item]
---         else
---             diff = amount
---         end
-
---         if (diff ~= nil and diff > 0) then
---             game.print(item)
---             game.print(diff)
---             mainInventory.remove({name=item, count=diff})
---             trashInventory.insert({name=item, count=diff})
---         end
---     end
+    local trashInventory = player.get_inventory(defines.inventory.character_trash)
+    local mainInventory = player.get_main_inventory()
 
 
---     playerInventories[event.player_index] = inventoryContent
--- end)
+    mainInventory.remove(event.item_stack)
+    trashInventory.insert(event.item_stack)
+end)
+
+script.on_event(defines.events.on_player_main_inventory_changed, function(event)
+    local player = game.get_player(event.player_index)
+    if not shortcutEnabled(player) then
+        return end
+
+    local trashInventory = player.get_inventory(defines.inventory.character_trash)
+    local mainInventory = player.get_main_inventory()
+    local inventoryContent = mainInventory.get_contents()
+    local diff = nil
+
+    for item, amount in pairs (inventoryContent) do
+        if playerInventory[item] ~= nil then
+            diff = amount - playerInventory[item]
+        else
+            diff = amount
+        end
+
+        if (diff ~= nil and diff > 0) then
+            game.print(item)
+            game.print(diff)
+            mainInventory.remove({name=item, count=diff})
+            trashInventory.insert({name=item, count=diff})
+        end
+    end
+
+    playerInventory = inventoryContent
+end)
 
 -- script.on_event(defines.events.on_player_created, function()
 --     game.print("dadw")
@@ -60,19 +72,12 @@
 -- end)
 
 script.on_event(defines.events.on_lua_shortcut, function(event)
-
     if (event.prototype_name ~= "trash-pickup-toggle") then
         return 
     end
-
     
     local player = game.get_player(event.player_index)
 
-    player.set_shortcut_toggled("trash-pickup-toggle", not player.is_shortcut_toggled("trash-pickup-toggle"))
-
-
-    game.print("Yaaay button toggled to")
-    game.print(player.is_shortcut_toggled("trash-pickup-toggle"))
+    player.set_shortcut_toggled("trash-pickup-toggle", not shortcutEnabled(player))
+    playerInventory = player.get_main_inventory().get_contents()
 end)
-
-
